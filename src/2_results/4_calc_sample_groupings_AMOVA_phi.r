b9fd5b90256048c885d1aa815e47a907064cf351
Max Hargreaves <whargrea@uoguelph.ca>
2019-01-11 12:17:37 -0500

    new amova results

--- src/2_results/4_calc_sample_groupings_AMOVA_phi.r
+++ src/2_results/4_calc_sample_groupings_AMOVA_phi.r
@@ -14,48 +14,48 @@ wheat_gds <- snpgdsOpen(
 sample_dist <- as.dist(1 - snpgdsIBS(wheat_gds, autosome.only = F)$ibs)
 snpgdsClose(wheat_gds)
 
-results <- tibble()
-for (stratum in names(strata_genind$strata)) {
+strata <- c("bp", "era", "bp_era", "pheno", "pheno/bp_era", "clusters", "clusters/bp_era")
+
+results <- tibble(
+  Comparison = character(), `% Variation` = double(), `p-value` = double()
+)
+for (stratum in strata) {
   print(stratum)
   amova_result <- poppr.amova(
     strata_genind, hier = str_c("~", stratum) %>% as.formula(), cutoff = 0.1,
     missing = "genotype", within = FALSE, clonecorrect = FALSE, 
     dist = sample_dist, quiet = T
   )
-  amova_randtest <- randtest(amova_result, nrepet = 999, alter = "greater")
-  results <- c(
-    stratum, round(amova_result$componentsofcovariance$`%`[1], 2),
-    amova_randtest$pvalue
-    ) %>%
-    rbind(results, ., stringsAsFactors = FALSE)
+  amova_randtest <- NULL
+  if (nrow(amova_result$statphi) == 1) {
+    amova_randtest <- randtest(amova_result, nrepet = 999, alter = "greater")
+    results <- results %>%
+      add_row(
+        Comparison = stratum,
+        `% Variation` = round(amova_result$componentsofcovariance$`%`[1], 2),
+        `p-value` = amova_randtest$pvalue
+      )
+  } else {
+    amova_randtest <- randtest(amova_result, nrepet = 999, output = "full") %>%
+    with(
+      as.krandtest(
+        sim, obs, alter = c("greater", "greater"), call = call,
+        names = names
+      )
+    )
+    results <- results %>%
+      add_row(
+        Comparison = str_c(stratum, "-", strsplit(stratum, "/")[[1]][1]),
+        `% Variation` = round(amova_result$componentsofcovariance$`%`[1], 2),
+        `p-value` = amova_randtest$pvalue[3]
+      )
+    results <- results %>%
+      add_row(
+        Comparison = str_c(stratum, "-", strsplit(stratum, "/")[[1]][2]),
+        `% Variation` = round(amova_result$componentsofcovariance$`%`[2], 2),
+        `p-value` = amova_randtest$pvalue[2]
+      )
+  }
 }
-names(results) <- c("Comparison", "Phi", "p-value")
-results
 
 write_csv(results, "Results//amova_results.csv", col_names = TRUE)
-
-poppr.amova(
-  strata_genind, hier = "~pheno" %>% as.formula(), cutoff = 0.1,
-  missing = "genotype", within = FALSE, clonecorrect = FALSE, 
-  dist = sample_dist, quiet = T
-)
-
-poppr.amova(
-  strata_genind, hier = "~bp_era" %>% as.formula(), cutoff = 0.1,
-  missing = "genotype", within = FALSE, clonecorrect = FALSE, 
-  dist = sample_dist, quiet = T
-) %>%
-randtest(nrepet = 999, output = "full", alter = "greater")
-
-poppr.amova(
-  strata_genind, hier = "~pheno/bp_era" %>% as.formula(), cutoff = 0.1,
-  missing = "genotype", within = FALSE, clonecorrect = FALSE, 
-  dist = sample_dist, quiet = T
-) %>%
-randtest(nrepet = 999, output = "full") %>%
-with(
-  as.krandtest(
-    sim, obs, alter = c("two-sided", "two-sided", "two-sided"),
-    call = call, names = names
-  )
-)
