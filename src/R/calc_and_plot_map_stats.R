88f60e1d3ac6fc561e5d18f27af2978b8c7ad445
Max H <whargrea@uoguelph.ca>
2019-04-28 14:31:32 -0400

    sumtin

--- src/R/calc_and_plot_map_stats.R
+++ src/R/calc_and_plot_map_stats.R
@@ -1,14 +1,15 @@
 # load file paths and functions
 source(file.path("src", "R", "file_paths.R"))
 import::from(pgda, "calc_plot_map_stats")
-# source(map_stats) # calc_plot_map_stats
+
+import::from(ggplot2, "scale_x_log10", "scale_y_log10")
 
 # all markers
 calc_plot_map_stats(
   phys_gds,
   "MAF and MR Pruned Marker Set",
-  1500,
-  basename(gds)
+  6000,
+  basename(phys_gds)
 )
 
 # ld pruned markers
@@ -16,5 +17,5 @@ calc_plot_map_stats(
   ld_gds,
   "MAF, MR, and LD Pruned Marker Set",
   500,
-  basename(gds)
+  basename(ld_gds)
 )
